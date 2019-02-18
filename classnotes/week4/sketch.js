8c1059cb56e0416efdeb043fb796fa0dafbb757d
lauren <lns44@miami.edu>
2019-02-18 15:56:22 -0500

    s

--- classnotes/week4/sketch.js
+++ classnotes/week4/sketch.js
@@ -20,7 +20,7 @@ Food = loadImage("Food.jpg");
 }
 function setup() {
   // put setup code here
-createCanvas(1000,1000);
+createCanvas(auto);
 
 TicketButton = createButton("Ticket");
 TicketButton.position(10,40);
