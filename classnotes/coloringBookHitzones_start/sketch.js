e94d8b5f525a5cd00c10efaa5cc08d231be1d02d
lauren <lns44@miami.edu>
2019-02-18 15:10:17 -0500

    d

--- classnotes/coloringBookHitzones_start/sketch.js
+++ classnotes/coloringBookHitzones_start/sketch.js
@@ -1,14 +1,19 @@
+var BrushColor = 0;
+var Brushsize= 10;
 
+var circleButtonX= 100;
+var circleButtonY= 
 function preload(){
 
 
 }
 function setup() {
   // put setup code here
-
+createCanvas(500,500);
 }
 
 function draw() {
   // put drawing code here
-
+Fill(BrushColor);
+ellipse(mouseX, mouseY, Brushsize, Brushsize);
 }
