6b23d5767f4c47b2788049502ce0ba4572a57f71
lauren <lns44@miami.edu>
2019-02-18 15:59:42 -0500

    a

--- classnotes/week4/sketch.js
+++ classnotes/week4/sketch.js
@@ -54,20 +54,6 @@ currentImage = 3;
 
 
 
-function draw() {
-  // put drawing code here
-if(currentImage == 0){
-  image(Ticket, 0,20, Ticket.width/2, Ticket.height/2);
-}else if(currentImage == 1){
-  image(Plane, 0, 20, Plane.width/2, Plane.height/2);
-}else if(currentImage == 2){
-  image(Temple, 0, 20, Temple.width/2, Temple.height/2);
-}
-else if(currentImage == 3){
-  image(Food, 0, 20,  Food.width/2, Food.height/2);
-}
-
-
 }
 
 
