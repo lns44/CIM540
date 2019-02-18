13cb1f982ae1341395d05be2cb507130e0c01be6
lauren <lns44@miami.edu>
2019-02-18 15:47:15 -0500

    s

--- classnotes/week4/sketch.js
+++ classnotes/week4/sketch.js
@@ -1,12 +1,12 @@
 /* "Up-Close Sloth" by marissa_strniste, "Baby sloth, being cute" by Dave Gingrich is licensed under CC BY-SA 2.0. To view a copy of this license, visit: https://creativecommons.org/licenses/by-sa/2.0*/
 
 var Ticket;
-var aussie2;
+var Plane;
 var aussie3;
 var aussie4;
 
 var TicketButton;
-var aussie2Button;
+var PlaneButton;
 var aussie3Button;
 var aussie4Button;
 
@@ -14,7 +14,7 @@ var currentImage = 0;
 
 function preload(){
 Ticket = loadImage("ticket.jpeg");
-aussie2 = loadImage("assets/aussie2.jpg");
+aussie2 = loadImage("Plane.jpeg");
 aussie3 = loadImage("assets/aussie3.jpg");
 aussie4 = loadImage("assets/aussie4.jpg");
 }
@@ -28,9 +28,9 @@ TicketButton.mousePressed(function(){
 currentImage = 0;
 });
 
-aussie2Button = createButton("aussie 2")
-aussie2Button.position(100,40);
-aussie2Button.mousePressed(aussieChange);
+PlaneButton = createButton("aussie 2")
+PlaneButton.position(100,40);
+PlaneButton.mousePressed(aussieChange);
 
 
 aussie3Button = createButton("aussie 3");
@@ -59,7 +59,7 @@ function draw() {
 if(currentImage == 0){
   image(ticket, 0,20, Ticket.width/2, Ticket.height/2);
 }else if(currentImage == 1){
-  image(aussie2, 0, 20, aussie2.width/2, aussie2.height/2);
+  image(aussie2, 0, 20, Plane.width/2, Plane.height/2);
 }else if(currentImage == 2){
   image(aussie3, 0, 20,  aussie3.width/2, aussie3.height/2);
 }
