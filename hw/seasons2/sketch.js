1f51680d7982e583cbbe2038112904274036b510
lauren <lns44@miami.edu>
2019-03-19 01:45:10 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -66,29 +66,30 @@ image(backImage,0,0, );
 
   if (seasonType == "Cherry Blossom Season") {
     // flower
-    int angle = 0;
 
-    void setup() {
-      size(640, 360);
-      background(102);
-      noStroke();
-      fill(0, 102);
+int angle = 0;
+
+void setup() {
+  size(640, 360);
+  background(102);
+  noStroke();
+  fill(0, 102);
+}
+
+void draw() {
+  // Draw only when mouse is pressed
+  if (mousePressed == true) {
+    angle += 5;
+    float val = cos(radians(angle)) * 12.0;
+    for (int a = 0; a < 360; a += 75) {
+      float xoff = cos(radians(a)) * val;
+      float yoff = sin(radians(a)) * val;
+      fill(0);
+      ellipse(mouseX + xoff, mouseY + yoff, val, val);
     }
-
-    void draw() {
-      // Draw only when mouse is pressed
-      if (mousePressed == true) {
-        angle += 5;
-        float val = cos(radians(angle)) * 12.0;
-        for (int a = 0; a < 360; a += 75) {
-          float xoff = cos(radians(a)) * val;
-          float yoff = sin(radians(a)) * val;
-          fill("pink");
-          ellipse(mouseX + xoff, mouseY + yoff, val, val);
-        }
-        fill("white"");
-        ellipse(mouseX, mouseY, 2, 2);
-      }
+    fill(255);
+    ellipse(mouseX, mouseY, 2, 2);
+  }
 
 } else if (seasonType == "Sun") {
     //suns out
