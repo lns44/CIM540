c1e5974b0014d01929f4a03196a572cf11e8aaf8
lauren <lns44@miami.edu>
2019-03-19 02:42:19 -0400

    s

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -1,5 +1,10 @@
 // leaf from open clip art https://openclipart.org/detail/257622/leaf
 
+let bug1;
+let bug2;
+let bug3;
+let bug4;
+
 var seasons;
 var seasonType = "";
 
@@ -33,10 +38,44 @@ function setup() {
   seasons.option("Sun");
   seasons.option("Leaves");
   seasons.option("Snow");
+bug1 = new Jitter();
+bug2 = new Jitter();
+bug3 = new Jitter();
+bug4 = new Jitter();
   seasons.changed(function() {
   seasonType = seasons.value();
   });
 
+  function draw() {
+    background(50, 89, 100);
+    bug1.move();
+    bug1.display();
+    bug2.move();
+    bug2.display();
+    bug3.move();
+    bug3.display();
+    bug4.move();
+    bug4.display();
+  }
+
+  // Jitter class
+class Jitter {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.diameter = random(10, 30);
+    this.speed = 1;
+  }
+
+  move() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+  display() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+}
 
   for (var i = 0; i < leafAmount; i++) {
     leafLocationX[i] = random(0, width);
