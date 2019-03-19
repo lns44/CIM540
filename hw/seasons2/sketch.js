8d6faa6e2577d5f8cfa203116e7050e3ff7aec1a
lauren <lns44@miami.edu>
2019-03-19 03:23:06 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -73,8 +73,7 @@ image(backImage,0,0, );
   if (seasonType == "Cherry Blossom Season") {
       ellipse(width / 2, height / 2, d, d);}
     function changeD() {
-      d = d + 10;
-    }}}
+      d=d + 10;}}}
 
 else if (seasonType == "Sun") {
 fill("yellow");
