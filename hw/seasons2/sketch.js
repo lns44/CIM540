6f46d1919576ccb5f6c6ba57a2b76102776b6dd5
lauren <lns44@miami.edu>
2019-03-19 05:20:12 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -95,7 +95,7 @@ ellipse(mouseX, mouseY, 70,70);
 }
  else if (seasonType == "Night or Day?") {
 
-   function button.Pressed(){
+   function buttonPressed(){
    fill("black");
    ellipse(50,50,50,50)}
    ;}
