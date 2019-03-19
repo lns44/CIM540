890633d06a72d53fe74dd2c00768fb38386d3f78
lauren <lns44@miami.edu>
2019-03-19 04:27:15 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -110,9 +110,27 @@ ellipse(mouseX, mouseY, 70,70);
         snowLocationX[i] = random(0,width);
       }
 else if (seasonType == "Night or Day?") {
-fill("yellow")
-ellipse(20,20,20,20)
-mousePressed(300)
+
+  function draw() {
+    background(127);
+    // Draw a circle
+    strokeWeight(2);
+    stroke(r, g, b);
+    fill(r, g, b, 127);
+    ellipse(360, 200, 200, 200);
+  }
+
+  // When the user clicks the mouse
+  function mousePressed() {
+    // Check if mouse is inside the circle
+    var d = dist(mouseX, mouseY, 360, 200);
+    if (d < 100) {
+      // Pick new random color values
+      r = random(255);
+      g = random(255);
+      b = random(255);
+    }
+  }
 
   }
 
