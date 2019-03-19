b190320a5e31b56153ab9f99018dcf9bf5f0ce61
lauren <lns44@miami.edu>
2019-03-19 03:58:45 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -68,9 +68,8 @@ image(backImage,0,0, );
 translate(580, 200);
 noStroke();
 for (let i = 0; i < 10; i ++) {
-ellipse(mouseX, 0, 30, 20, 80);
-rotate(PI/5);   // Top circle
-  ellipse(mouseX+30, 50, 33, 33); // Middle circle
+ellipse(mouseX, 0, 30, 20, 80);// Top circle
+ellipse(mouseX+30, 50, 33, 33); // Middle circle
 ellipse(mouseX-60, 84, 33, 33); // Bottom circle
   }
 
