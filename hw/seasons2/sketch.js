1ea4a6a8cfeff0663e8aa91966a492ced586a9e2
lauren <lns44@miami.edu>
2019-03-19 16:05:30 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -58,7 +58,7 @@ image(backImage,0,0, );
   textSize(20);
   text(seasonType, 130, 55);
 
-  if (seasonType == "Cherry Blossoms")  {
+if (seasonType == "Cherry Blossoms")  {
 image(BlosImage, mouseX, 20, 40, 40);  // Top circle
 image(BlosImage, mouseX+10, 15, 40, 40);
 image(BlosImage, mouseX-10, 15, 40, 40);
