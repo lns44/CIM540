0e054697d5e0760b482dc31ceb2b607563dd7e0e
lauren <lns44@miami.edu>
2019-03-19 03:08:04 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -65,7 +65,7 @@ image(backImage,0,0, );
 
   if (seasonType == "Cherry Blossom Season") {
     function mousePressed() {
-      image(blosImage; mouseX, mouseY, 33, 33);
+      blosImageimage(mouseX, mouseY, 33, 33);
     }}
 
 
