33c23d02aee7590d34873b44cd68e2fb6045c6ca
lauren <lns44@miami.edu>
2019-03-19 03:55:29 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -65,7 +65,12 @@ image(backImage,0,0, );
   text(seasonType, 120, 55);
 
   if (seasonType == "Cherry Blossom Season") {
-    blos(mouseX, 200, 33);    // Top circle
+translate(580, 200);
+noStroke();
+for (let i = 0; i < 10; i ++) {
+ellipse{(0, 30, 20, 80);
+rotate(PI/5);
+(mouseX, 200, 33)};    // Top circle
     ellipse(mouseX+30, 50, 33, 33); // Middle circle
     ellipse(mouseX-60, 84, 33, 33); // Bottom circle
   }
