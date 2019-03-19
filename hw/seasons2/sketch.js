6315ad10f2fe85e5ee76b2d7fd54da2608069ca7
lauren <lns44@miami.edu>
2019-03-19 01:52:44 -0400

    s

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -37,7 +37,7 @@ function setup() {
   seasons.option("Leaves");
   seasons.option("Snow");
   seasons.changed(function() {
-  seasonType = seasons.value();
+    seasonType = seasons.value();
   });
 
   //seasonType = "winter";
@@ -68,14 +68,13 @@ image(backImage,0,0, );
     // flower
     fill("pink");
 
-    function mousePressed() {
-      var i = 0; i < 12; i++) {
+    for (var i = 0; i < 12; i++) {
       push();
       translate(width / 2, height / 2);
       rotate(i + 30);
       scale(1.5);
       ellipse(0, 10, 10, 20);
-      pop(); (mouseX, mouseY, 33, 33);
+      pop();
     }
     fill("white");
     ellipse(width / 2, height / 2, 20, 20);
