e811935b9d6076e9df623caab2e68f250972ac19
lauren <lns44@miami.edu>
2019-03-19 01:23:24 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -62,7 +62,7 @@ image(backImage,0,0, );
   fill("black");
   stroke(0);
   textSize(20);
-  text(seasonType, 10, 75);
+  text(seasonType, 10, 135);
 
   if (seasonType == "Cherry Blossoms") {
     // flower
