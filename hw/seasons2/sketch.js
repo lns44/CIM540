182b74dc7922f8656813fc132563ca89f18c04e2
lauren <lns44@miami.edu>
2019-03-19 03:44:02 -0400

    s

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -67,15 +67,16 @@ image(backImage,0,0, );
 
     fill("pink");
 
-           for (var i = 0; i < 12; i++) {
-             push();
-             rotate(i + 30);
-             scale(1.5);
-             ellipse(0,20,35,300);
-             pop();
-           }
-           fill("white");
-           ellipse(350, 300, 30, 30);}
+ for (var i = 0; i < 12; i++) {
+   push();
+   translate(width / 2, height / 2);
+   rotate(i + 30);
+   scale(1.5);
+   ellipse(0, 10, 10, 20);
+   pop();
+ }
+ fill("white");
+ellipse(350, 300, 30, 30);}
 
 
 
