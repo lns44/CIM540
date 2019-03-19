714ec1e2400886614434f823782035757deab5cf
lauren <lns44@miami.edu>
2019-03-19 01:23:09 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -68,7 +68,7 @@ image(backImage,0,0, );
     // flower
     fill("pink");
 
-    for (var i = 0; i < 14; i++) {
+    for (var i = 0; i < 12; i++) {
       push();
       translate(width / 2, height / 2);
       rotate(i + 30);
