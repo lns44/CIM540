d30e895ece07dc9eb2853c24f5f2d420d6b8ca62
lauren <lns44@miami.edu>
2019-03-19 01:21:19 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -1,63 +1,118 @@
+// leaf from open clip art https://openclipart.org/detail/257622/leaf
+var seasons;
+var seasonType = "";
+
+var sunX = 360;
+var sunY = 40;
+var sunSize = 60;
+
+var leafLocationX = [];
+var leafLocationY = [];
+var leafImage;
+var leafAmount = 100;
+
+var snowLocationX = [];
+var snowLocationY = [];
+var snowAmount = 100;
+
+var backImage;
+var snowimage;
+
+let snowimage;
+function preload() {
+leafImage = loadImage("https://openclipart.org/image/300px/svg_to_png/257622/1470680735.png");
+backImage = loadImage("assets/mnt.jpg");
+snowimage= loadImage("assets/snow.jpg");
+}
+
 
 function setup() {
   // put setup code here
-  createCanvas(400,400);
-
-  button1 = createButton("1");
-  button1.position(10,300);
-  button1.mousePressed(function(){
-    currentOption = 1;
+  createCanvas(400, 400);
+  seasons = createSelect();
+  seasons.position(130, 10);
+  seasons.option("Click Here!");
+  seasons.option("Cherry Blossoms");
+  seasons.option("Sun");
+  seasons.option("Leaves");
+  seasons.option("Snow");
+  image(snowimage, 0, 0);
+  seasons.changed(function() {
+  seasonType = seasons.value();
+  currentImage = 0;
   });
 
-  button2 = createButton("2");
-  button2.position(50,300);
-  button2.mousePressed(function(){
-    currentOption = 2;
-  });
+  //seasonType = "winter";
 
-  button3 = createButton("3");
-  button3.position(100,300);
-  button3.mousePressed(function(){
-    currentOption = 3;
-  });
+  for (var i = 0; i < leafAmount; i++) {
+    leafLocationX[i] = random(0, width);
+    leafLocationY[i] = random(0, -500);
+  }
 
-  button4 = createButton("4");
-  button4.position(150,300);
-  button4.mousePressed(function(){
-    currentOption = 4;
-  });
+  for (var i = 0; i < snowAmount; i++) {
+    snowLocationX[i] = random(0, width);
+    snowLocationY[i] = random(0, -500);
+  }
 
 }
 
 function draw() {
   // put drawing code here
-  console.log(currentOption);
-  background(255);
-  text(questions[currentQuestion], 20,100);
+  //console.log(frameRate());
+background(255);
+image(backImage,0,0, );
+  fill("black");
+  stroke(0);
+  textSize(20);
+  text(seasonType, 10, 75);
 
-  text(options[currentQuestion], 20,150);
+  if (seasonType == "Cherry Blossoms") {
+    // flower
+    fill("pink");
 
-  text(answerCorrect, 20,200);
+    for (var i = 0; i < 12; i++) {
+      push();
+      translate(width / 2, height / 2);
+      rotate(i + 30);
+      scale(2.0);
+      ellipse(0, 10, 10, 20);
+      pop();
+    }
+    fill("white");
+    ellipse(width / 2, height / 2, 20, 20);
 
-  if(currentOption != -1){
+  } else if (seasonType == "Sun") {
+    //suns out
+    fill("orange");
+    ellipse(sunX, sunY, sunSize, sunSize);
+    if (sunY > 350) {
+      sunY = sunY - 5;
 
-    if(currentOption == answers[currentQuestion]){
-      // correct
-      console.log("correct");
-      answerCorrect = "Correct";
-      currentQuestion = currentQuestion + 1;
+    }
+  } else if (seasonType == "Leaves") {
+    for (var i = 0; i < leafAmount; i++) {
+      console.log("leaf" + 0 + " x: " + leafLocationX[1] + " , y: " + leafLocationY[1]);
+      image(leafImage, leafLocationX[i], leafLocationY[i], 20,20);
 
-      if(currentQuestion >= questions.length){
-        currentQuestion = 0;
+      if(leafLocationY[i] < height - 20){
+        leafLocationY[i]++;
+        leafLocationX[i] = leafLocationX[i] + sin(radians(frameCount));
       }
 
-    }else{
-      //incorrect
-      console.log("incorrect");
-      answerCorrect = "Try again!";
     }
-    currentOption = -1;
+
+  } else if (seasonType == "Snow") {
+
+    noStroke();
+    fill(173);
+    for (var i = 0; i < snowAmount; i++) {
+      snowLocationY[i]++;
+      ellipse(snowLocationX[i], snowLocationY[i], 5,5);
+
+      if(snowLocationY[i] > height){
+        snowLocationY[i] = random(-100,0);
+        snowLocationX[i] = random(0,width);
+      }
+    }
   }
-
-
 }
