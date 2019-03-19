722d0eba37d8caa6e8be35d144c94461908111b9
lauren <lns44@miami.edu>
2019-03-19 01:34:04 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -72,7 +72,7 @@ image(backImage,0,0, );
       push();
       translate(width / 2, height / 2);
       rotate(i + 30);
-      scale(2.0);
+      scale(1.5);
       ellipse(0, 10, 10, 20);
       pop();
     }
