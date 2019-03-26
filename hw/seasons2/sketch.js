6d5debc900ea15b9af15a554151096cd9615f3d1
lauren <lns44@miami.edu>
2019-03-26 17:50:57 -0400

    k

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -94,7 +94,7 @@ ellipse(mouseX, mouseY, 70,70);
   }
   if(mouseX< height/2){
     console.log("Above Line");
-    image(sunImage, mouseX, 20, 150, 150);}
+    image(sunImage, mouseX, 20, 150, 150)};
     image(topImage.width/6,topImage.height/6);
 
  }else if (seasonType == "Snow") {
