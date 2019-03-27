e1d45970e9bc1ab82145709ae37124d6d4fb1192
lauren <lns44@miami.edu>
2019-03-27 17:53:32 -0400

    a

--- hw/interactivecharacter/sketch.js
+++ hw/interactivecharacter/sketch.js
@@ -53,7 +53,7 @@ ellipse(350,135,25,30);
   ellipse(250,135,10,10);
   fill('black');
   textSize(12);
-  text('click and hold to change the background red!', 900, 250);
+  text('click and hold to change the background red!', 700, 250);
 
   //mouse
 
