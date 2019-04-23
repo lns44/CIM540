36d90e57adeb6d44ae5a8a759dabb72308c07062
lauren <lns44@miami.edu>
2019-04-23 15:57:00 -0400

    j

--- hw/finalproject/sketch.js
+++ hw/finalproject/sketch.js
@@ -2,17 +2,13 @@ function preload(){
 background= loadImage("assets/background.jpg");}
 
 
-
 function setup() {
   // put setup code here
   createCanvas(1024,1366);
-
 }
 
 function draw() {
-
 background(255);
 image(midtermicons2, 0,0, midtermicons2.width/6, midtermicons2.height/6);
 
-  // put drawing code here
 }
