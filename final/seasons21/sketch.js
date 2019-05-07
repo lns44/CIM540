3aee4338e3514a31951547c8e08549e828425360
lauren <lns44@miami.edu>
2019-05-06 23:29:45 -0400

    s

---
+++ final/seasons21/sketch.js
@@ -0,0 +1,25 @@
+let bg;
+let y = 0;
+var snake;
+var manatee;
+var deer;
+var fish;
+var cougar;
+var croc;
+var fruit;
+
+var snakebutton;
+var manateebutton;
+var deerbutton;
+var fishbutton;
+var cougarbutton;
+var crocbutton;
+
+
+
+function setup() {
+bg = loadImage("assets/background.jpg");
+createCanvas(800, 600);
+
+
+}
