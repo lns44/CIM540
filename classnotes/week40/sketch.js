dc715cefe3dbeec2184ccf1c492e9bebb77acbae
lauren <lns44@miami.edu>
2019-05-07 01:12:10 -0400

    a

--- classnotes/week40/sketch.js
+++ classnotes/week40/sketch.js
@@ -52,7 +52,7 @@ function setup() {
     snowLocationY[i] = random(0, -500);}}
 
 function draw() {
-background(300);
+background(400);
 
 image(backImage,0,0, backImage.width/3,backImage.height/3);
 
