6e0e68d26cd150081758b2ca9fe0e653f5ea15aa
lauren <lns44@miami.edu>
2019-05-07 00:44:38 -0400

    a

--- final/seasons21/sketch.js
+++ final/seasons21/sketch.js
@@ -1,15 +1,126 @@
-var snake;
-var background;
+var seasons;
+var seasonType = "";
 
-var snakebutton;
+var sunX = 360;
+var sunY = 40;
+var sunSize = 60;
 
-var currentImage = 0;
+var leafLocationX = [];
+var leafLocationY = [];
+var leafImage;
+var leafAmount = 100;
 
-function preload(){
-background = loadImage("assets/background.jpg");
+var snowLocationX = [];
+var snowLocationY = [];
+var snowAmount = 150;
+
+var backImage;
+
+function preload() {
+leafImage = loadImage("assets/leaf.png");
+backImage = loadImage("assets/background.jpg");
+BlosImage = loadImage("assets/blos.png");
+moonImage = loadImage("assets/moon.png");
+sunImage = loadImage("assets/sun.png");
+topImage = loadImage("assets/mnttop.png");
 }
+
 function setup() {
-createCanvas(800,600);
-background = loadImage("assets/background.jpg");
-currentImage = 0;
-});
+  // put setup code here
+  createCanvas(800,600);
+  noStroke(0);
+  seasons = createSelect();
+  seasons.position(17, 17,50);
+  seasons.option("Click here to change up the scene!");
+  seasons.option("Cherry Blossoms");
+  seasons.option("Sun");
+  seasons.option("Leaves");
+  seasons.option("Let it Snow!");
+  seasons.option("Day to Night");
+  canvas = createCanvas(800,600);
+  seasons.changed(function() {
+  seasonType = seasons.value();
+  });
+
+  for (var i = 0; i < leafAmount; i++) {
+    leafLocationX[i] = random(0, width);
+    leafLocationY[i] = random(0, -500);
+  }
+
+  for (var i = 0; i < snowAmount; i++) {
+    snowLocationX[i] = random(0, width);
+    snowLocationY[i] = random(0, -500);}}
+
+function draw() {
+background(255);
+image(backImage);
+
+
+if (seasonType == "Click here to change up the scene!") {
+  textSize(14);
+  text('Welcome to Japan!',350, 30);
+  text('Click the dropdown bar on the left to change of the scene!', 250, 50);
+
+
+}if (seasonType == "Cherry Blossoms") {
+  textSize(12);
+  text('Move your mouse and see if you can align the cherry blossoms above the mountain top!',190,80);
+
+image(BlosImage, mouseX, 20, 40, 40);  // Top circle
+image(BlosImage, mouseX+10, 15, 40, 40);
+image(BlosImage, mouseX-10, 15, 40, 40);
+image(BlosImage, mouseX+30, 40, 40, 40);
+image(BlosImage, mouseX-30, 40, 40, 40);
+image(BlosImage, mouseX+70, 60, 40, 40);
+image(BlosImage, mouseX-70, 60, 40, 40);
+image(BlosImage, mouseX+120, 90, 40, 40);
+image(BlosImage, mouseX-120, 90, 40, 40);
+image(BlosImage, mouseX+150, 110, 40, 40);
+image(BlosImage, mouseX-150, 110, 40, 40);// Bottom circle
+
+  }else if (seasonType == "Sun") {
+    textSize(12);
+    text('Move your mouse around and the sun will follow!', 300, 70);
+    fill("yellow");
+    ellipse(mouseX, mouseY, 100,100);
+
+  } else if (seasonType == "Leaves") {
+    textSize(12);
+      text('It is fall in beautiful Japan!', 340, 70);
+    for (var i = 0; i < leafAmount; i++) {
+      console.log("leaf" + 0 + " x: " + leafLocationX[1] + " , y: " + leafLocationY[1]);
+      image(leafImage, leafLocationX[i], leafLocationY[i], 20,20);
+
+      if(leafLocationY[i] < height - 20){
+        leafLocationY[i]++;
+        leafLocationX[i] = leafLocationX[i] + sin(radians(frameCount));}}
+}else if (seasonType == "Day to Night") {
+  textSize(12);
+  text('Move your mouse from left to right to change the time from day to night!', 250, 80);
+
+  if(mouseX> height/2){
+    console.log("Below Line");
+    image(moonImage, mouseX, 20, 150, 150);
+  }
+  if(mouseX< height/2){
+    console.log("Above Line");
+
+    image(sunImage, mouseX, 20, 150, 150)};
+    image(topImage,276,94,260,100);
+
+ }else if (seasonType == "Let it Snow!") {
+   textSize(12);
+   text('Let it snow, let it snow, let it snow!', 320, 70);
+  noStroke();
+  fill(230);
+  for (var i = 0; i < snowAmount; i++) {
+    snowLocationY[i]++;
+    ellipse(snowLocationX[i], snowLocationY[i], 4,4);
+
+    if(snowLocationY[i] > height){
+      snowLocationY[i] = random(-50,0);
+      snowLocationX[i] = random(0,width);}
+
+    }
+  }
+}
--- hw/empty-example0/sketch.js
+++ hw/empty-example0/sketch.js
@@ -1,73 +1,133 @@
+var seasons;
+var seasonType = "";
 
+var sunX = 360;
+var sunY = 40;
+var sunSize = 60;
 
-var eyePosX = 0;
-var eyePosY = 0;
-var eyeOffset = 20;
-var eyeSize = 20;
+var leafLocationX = [];
+var leafLocationY = [];
+var leafImage;
+var leafAmount = 100;
 
-var backgroundColor = "white";
+var snowLocationX = [];
+var snowLocationY = [];
+var snowAmount = 150;
 
-var instruction = "Move your mouse, Don't make the face sad.";
+var backImage;
+
+function preload() {
+leafImage = loadImage("assets/leaf.png");
+backImage = loadImage("assets/background.jpg");
+BlosImage = loadImage("assets/blos.png");
+moonImage = loadImage("assets/moon.png");
+sunImage = loadImage("assets/sun.png");
+topImage = loadImage("assets/mnttop.png");
+}
 
 function setup() {
   // put setup code here
-  createCanvas(500,500);
-  eyePosX = width/2;
-  eyePosY = 100;
-}
+  createCanvas(900,700);
+  noStroke(0);
+  seasons = createSelect();
+  seasons.position(17, 17,50);
+  seasons.option("Click here to change up the scene!");
+  seasons.option("Cherry Blossoms");
+  seasons.option("Sun");
+  seasons.option("Leaves");
+  seasons.option("Let it Snow!");
+  seasons.option("Day to Night");
+  canvas = createCanvas(900,700);
+  seasons.changed(function() {
+  seasonType = seasons.value();
+  });
+
+  for (var i = 0; i < leafAmount; i++) {
+    leafLocationX[i] = random(0, width);
+    leafLocationY[i] = random(0, -500);
+  }
+
+  for (var i = 0; i < snowAmount; i++) {
+    snowLocationX[i] = random(0, width);
+    snowLocationY[i] = random(0, -500);}}
 
 function draw() {
-  // put drawing code here
-  background(backgroundColor);
+background(255);
+image(backImage,0,0, backImage.width/6,backImage.height/6);
+
   fill("black");
-  text(instruction, 20, 50);
-  ellipse(mouseX,mouseY,10,10);
-  ellipse(eyePosX - eyeOffset, eyePosY, eyeSize,eyeSize);
-  ellipse(eyePosX + eyeOffset, eyePosY, eyeSize,eyeSize);
+  stroke(0);
+  textSize(14);
+  textSize(14);
+  text('Welcome to Japan!',350, 30);
+  text('Click the dropdown bar on the left to change of the scene!', 250, 50);
 
-  line(0, height/2, width, height/2);
 
-  if(mouseY > height/2){
+if (seasonType == "Click here to change up the scene!") {
+  textSize(14);
+  text('Welcome to Japan!',350, 30);
+  text('Click the dropdown bar on the left to change of the scene!', 250, 50);
+
+
+}if (seasonType == "Cherry Blossoms") {
+  textSize(12);
+  text('Move your mouse and see if you can align the cherry blossoms above the mountain top!',190,80);
+
+image(BlosImage, mouseX, 20, 40, 40);  // Top circle
+image(BlosImage, mouseX+10, 15, 40, 40);
+image(BlosImage, mouseX-10, 15, 40, 40);
+image(BlosImage, mouseX+30, 40, 40, 40);
+image(BlosImage, mouseX-30, 40, 40, 40);
+image(BlosImage, mouseX+70, 60, 40, 40);
+image(BlosImage, mouseX-70, 60, 40, 40);
+image(BlosImage, mouseX+120, 90, 40, 40);
+image(BlosImage, mouseX-120, 90, 40, 40);
+image(BlosImage, mouseX+150, 110, 40, 40);
+image(BlosImage, mouseX-150, 110, 40, 40);// Bottom circle
+
+  }else if (seasonType == "Sun") {
+    textSize(12);
+    text('Move your mouse around and the sun will follow!', 300, 70);
+    fill("yellow");
+    ellipse(mouseX, mouseY, 100,100);
+
+  } else if (seasonType == "Leaves") {
+    textSize(12);
+      text('It is fall in beautiful Japan!', 340, 70);
+    for (var i = 0; i < leafAmount; i++) {
+      console.log("leaf" + 0 + " x: " + leafLocationX[1] + " , y: " + leafLocationY[1]);
+      image(leafImage, leafLocationX[i], leafLocationY[i], 20,20);
+
+      if(leafLocationY[i] < height - 20){
+        leafLocationY[i]++;
+        leafLocationX[i] = leafLocationX[i] + sin(radians(frameCount));}}
+}else if (seasonType == "Day to Night") {
+  textSize(12);
+  text('Move your mouse from left to right to change the time from day to night!', 250, 80);
+
+  if(mouseX> height/2){
     console.log("Below Line");
-    instruction = "I am sad!";
-    fill(255,0,0);
-    angleMode(DEGREES);
-    arc(width/2, 200, 100,100, 180,0);
+    image(moonImage, mouseX, 20, 150, 150);
   }
-
-  if(mouseY < height/2){
+  if(mouseX< height/2){
     console.log("Above Line");
-    instruction = "Move your mouse, Don't make the face sad.";
-    fill(0,0,0);
-    angleMode(DEGREES);
-    arc(width/2, 200, 100,100, 0,180);
-  }
 
-  //Or combine them
-  /*
-  if(mouseY > height/2){
-      console.log("Below Line");
-  }else{
-    console.log("Above Line");
-  }
-  */
-}
+    image(sunImage, mouseX, 20, 150, 150)};
+    image(topImage,276,94,260,100);
 
-function mousePressed(){
-  backgroundColor = "black";
-}
+ }else if (seasonType == "Let it Snow!") {
+   textSize(12);
+   text('Let it snow, let it snow, let it snow!', 320, 70);
+  noStroke();
+  fill(230);
+  for (var i = 0; i < snowAmount; i++) {
+    snowLocationY[i]++;
+    ellipse(snowLocationX[i], snowLocationY[i], 4,4);
 
-function mouseReleased(){
-  backgroundColor = "white";
-}
+    if(snowLocationY[i] > height){
+      snowLocationY[i] = random(-50,0);
+      snowLocationX[i] = random(0,width);}
 
-function keyPressed(){
-  console.log("Current key: " + key + " : " + keyCode);
-  if(key == "r"){
-    backgroundColor = "red";
-  }else if(key == "g"){
-    backgroundColor = "green";
-  }else if(key == "b"){
-    backgroundColor = "blue";
+    }
   }
 }
