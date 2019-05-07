a7a72c02542a2ed4b2ccb8dc812627c887e69840
lauren <lns44@miami.edu>
2019-05-06 23:15:37 -0400

    a

--- hw/seasons21/sketch.js
+++ hw/seasons21/sketch.js
@@ -24,11 +24,5 @@ loadImage("assets/snake.png");
 function setup() {
 createCanvas(800, 600);
 bg = loadImage("assets/background.jpg");
-button = createButton ('Snake');
-button.position(100,100);
-button.mousePressed(changeBG);}
 
-function changeBG(){
-  var val = loadImage("assets/snake.png");
-  background(val);
 }
