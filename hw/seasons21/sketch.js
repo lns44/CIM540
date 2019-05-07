70e1c3b6e2354a5a45598cb234d11e47bb0aca0c
lauren <lns44@miami.edu>
2019-05-06 22:17:18 -0400

    a

--- hw/seasons21/sketch.js
+++ hw/seasons21/sketch.js
@@ -19,7 +19,7 @@ var fruitbutton;
 
 var currentImage = 0;
 
-var fruitY = 45;
+var fruitY = 193;
 
 var snakeArray = [];
 
@@ -30,16 +30,28 @@ var fruitOffY = 0;
 
 function preload(){
 snakeArray[0] = loadImage("assets/snake.png");
+empty = loadImage("assets/empty.png");
 }
 
 
 function setup() {
 bg = loadImage("assets/background.jpg");
 createCanvas(800, 600);
+fruit = empty;
+
+reset = createButton("Start Over");
+reset.position(70,620);
+reset.mousePressed(function(){
+fruit = empty;
  }
 
 
 function draw() {
 background(bg);
-
+if mousePressed
+if(mouseX > 64 && mouseX < 85 && mouseY > 185 && mouseY < 215){
+  if(mouseIsPressed == true){
+    fruit = snakeArray[0];
+    fruitOffX = 70;
+    fruitOffY = 25;
  }
