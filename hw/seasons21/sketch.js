3d6ae2e16eebd1e1709baf2ae9bb01f5a3cae9fa
lauren <lns44@miami.edu>
2019-05-06 20:04:29 -0400

    a

--- hw/seasons21/sketch.js
+++ hw/seasons21/sketch.js
@@ -9,17 +9,5 @@ backImage = loadImage("assets/background.jpg");
 function setup() {
   // put setup code here
   createCanvas(900,700);
-  noStroke(0);
-
-  canvas = createCanvas(900,700);
 
   });
-
-
-function draw() {
-background(255);
-image(backImage,0,0, backImage.width/6,backImage.height/6);
-
-
-
-)};
