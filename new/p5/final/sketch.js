473239da9c2499653238b3fcc4abaaa04ad020c3
lauren <lns44@miami.edu>
2019-05-07 00:34:58 -0400

    a

--- new/p5/final/sketch.js
+++ new/p5/final/sketch.js
@@ -1,7 +1,9 @@
 function setup() {
 createCanvas(800,600);
+background ("assets/background");
 }
 
 function draw() {
-  // put drawing code here
+loadImage("assets/background.jpg");
+createCanvas(800, 600);
 }
