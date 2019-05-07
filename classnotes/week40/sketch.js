c58b2c95f8388f4d06442ed3dfa7b0bc4eb7ad83
lauren <lns44@miami.edu>
2019-05-07 11:12:09 -0400

    a

--- classnotes/week40/sketch.js
+++ classnotes/week40/sketch.js
@@ -1,139 +1,65 @@
-let song;
+var song;
 
 var seasons;
 var seasonType = "";
 
 var answer;
-var Python;
-
-var PythonButton;
-
-
 var python;
+var page = 0;
 
-var answerButton;
-var pythonButton;
-
-var currentImage = 0;
-
-var leafLocationX = [];
-var leafLocationY = [];
-var leafImage;
-var leafAmount = 100;
-
-var bubbleLocationX = [];
-var bubbleLocationY = [];
-var bubbleImage;
-var bubbleAmount = 100;
-
-var snowLocationX = [];
-var snowLocationY = [];
-var snowAmount = 0;
-
-var backImage;
+var currentText = "";
 
 function preload() {
-bubbleImage = loadImage("assets/bubble.png");
-leafImage = loadImage("assets/leaf.png");
+
 backImage = loadImage("assets/mnt.jpg");
-pythonArray[0] = loadImage("assets/python.png");
-Python = loadImage("assets/python.png");
+python = loadImage("assets/python.jpeg");
+
+soundFormats('mp3');
+song = loadSound('assets/rattlesnakerattle.mp3');
+
 }
 
 function setup() {
   // put setup code here
-  song = loadSound('assets/snakehiss.mp3');
   createCanvas(1440,754);
   noStroke(0);
-  seasons = createSelect();
-  seasons.position(17, 17,50);
-  textSize(20);
-  seasons.option("Click Here To Change The Environment!");
-  seasons.option("Leaves");
-  seasons.option("Bubbles");
-  canvas = createCanvas(1440,754);
-  seasons.changed(function() {
-  seasonType = seasons.value();
 
-answer = empty;
 
-  });
-
-  PythonButton = createButton("Click Me!");
-  PythonButton.position(150,305, 518,155);
-  PythonButton.mousePressed(function(){
-loadImage("assets/python.png");
-  });
-
+}
 
   function mousePressed() {
-    if (song.isPlaying()) {
-      song.stop();
-      background(0,100);
-    } else {
+    // if (song.isPlaying()) {
+    //   song.stop();
+    //   background(0,100);
+    // }else{
+    //   song.play();
+    //   background(64, 275, 518, 155);
+    // }
+
+    //145 197 177 155
+
+    if(mouseX > 145 && mouseX < (145 + 177) && mouseY > 197 && mouseY < (197 + 155)){
+      page = 1;
+      currentText = "Burmese Python: Invasive Species";
       song.play();
-      background(64, 275, 518, 155);
     }
   }
 
-  for (var i = 0; i < leafAmount; i++) {
-    leafLocationX[i] = random(0, width);
-    leafLocationY[i] = random(0, -500);
-  }
-
-  for (var i = 0; i < bubbleAmount; i++) {
-    bubbleLocationX[i] = random(0, width);
-    bubbleLocationY[i] = random(0, -400);
-  }
-
-  for (var i = 0; i < snowAmount; i++) {
-    snowLocationX[i] = random(0, width);
-    snowLocationY[i] = random(0, -500);}}
 
 function draw() {
 background(255);
 image(backImage,0,0, backImage.width/4.5,backImage.height/4.5);
+textSize(25);
+fill('red');
+text(currentText, 240,170);
 
 if(page == 1){
-  image(mainpage, 0,0, mainpage.width, mainpage.height);
 
-  if(mouseX > 64 && mouseX < (275) && mouseY > 518 && mouseY < 155){
-    if(mouseIsPressed == true){
-      answer = pythonArray[0];}
-    }
+  // show snake image
+  image(python, 650,220, 620, 420 );
+  //717 627 256 420
 
 //leaves
-if (seasonType == "Click here to change up the scene!") {
+}
 
-
-}if (seasonType == "Leaves") {
-    for (var i = 0; i < leafAmount; i++) {
-      console.log("leaf" + 0 + " x: " + leafLocationX[1] + " , y: " + leafLocationY[1]);
-      image(leafImage, leafLocationX[i], leafLocationY[i], 20,20);
-
-    }if(leafLocationY[i] < height - 20){
-        leafLocationY[i]++;
-        leafLocationX[i] = leafLocationX[i] + sin(radians(frameCount));}}
-//end of leaves
-
-  } else if (seasonType == "Bubbles") {
-    textSize(12);
-    fill("black");
-    for (var i = 0; i < bubbleAmount; i++) {
-      console.log("bubble" + 0 + " x: " + bubbleLocationX[1] + " , y: " + bubbleLocationY[1]);
-      image(bubbleImage, bubbleLocationX[i], bubbleLocationY[i], 30,30);
-
-    }if(bubbleLocationY[i] < height - 20){
-        bubbleLocationY[i]++;
-        bubbleLocationX[i] = bubbleLocationX[i] + sin(radians(frameCount));}}
-
-
- else if (seasonType == "Let it Snow!") {
-  for (var i = 0; i < snowAmount; i++) {
-    snowLocationY[i]++;
-    ellipse(snowLocationX[i], snowLocationY[i], 4,4);
-
-  }if(snowLocationY[i] > height){
-      snowLocationY[i] = random(-50,0);
-      snowLocationX[i] = random(0,width);}
-}}
+}
