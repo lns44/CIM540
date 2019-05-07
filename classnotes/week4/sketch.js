c5284cc36a6cfa0cd0dc42ce245a0ad7b23cb7c6
lauren <lns44@miami.edu>
2019-05-07 09:29:15 -0400

    a

--- classnotes/week4/sketch.js
+++ classnotes/week4/sketch.js
@@ -1,18 +1,18 @@
-var ticket;
-var plane;
+var Ticket;
+var Plane;
 var Temple;
 var Food;
 
-var ticketButton;
-var planeButton;
+var TicketButton;
+var PlaneButton;
 var TempleButton;
 var FoodButton;
 
 var currentImage = 0;
 
 function preload(){
-ticket = loadImage("ticket.jpg");
-plane = loadImage("plane.jpg");
+Ticket = loadImage("ticket.jpg");
+Plane = loadImage("plane.jpg");
 Temple = loadImage("Temple.jpg");
 Food = loadImage("Food.jpg");
 }
@@ -20,15 +20,15 @@ function setup() {
   // put setup code here
 createCanvas(500,800);
 
-ticketButton = createButton("Ticket");
-ticketButton.position(10,40);
-ticketButton.mousePressed(function(){
+TicketButton = createButton("Ticket");
+TicketButton.position(10,40);
+TicketButton.mousePressed(function(){
 currentImage = 0;
 });
 
-planeButton = createButton("Plane")
-planeButton.position(100,40);
-planeButton.mousePressed(Change);
+PlaneButton = createButton("Plane")
+PlaneButton.position(100,40);
+PlaneButton.mousePressed(Change);
 
 
 TempleButton = createButton("Temple");
@@ -55,9 +55,9 @@ currentImage = 3;
 function draw() {
   // put drawing code here
 if(currentImage == 0){
-  image(ticket, 0,20, ticket.width/2, ticket.height/2);
+  image(Ticket, 0,20, Ticket.width/2, Ticket.height/2);
 }else if(currentImage == 1){
-  image(Plane, 0, 20, plane.width/2, plane.height/2);
+  image(Plane, 0, 20, Plane.width/2, Plane.height/2);
 }else if(currentImage == 2){
   image(Temple, 0, 20, Temple.width/2, Temple.height/2);
 }
