7c5e09e986ec152bfdb711bcf274185b6317f72f
lauren <lns44@miami.edu>
2019-05-07 01:18:34 -0400

    a

--- hw/seasons2/sketch.js
+++ hw/seasons2/sketch.js
@@ -53,7 +53,7 @@ function setup() {
 
 function draw() {
 background(255);
-image(backImage,0,0, backImage.width/6,backImage.height/6);
+image(backImage,0,0, backImage.width,backImage.height);
 
   fill("black");
   stroke(0);
