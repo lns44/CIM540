8428cc5b812f3877e8a315c7c7410064c7257a90
lauren <lns44@miami.edu>
2019-05-07 01:07:06 -0400

    a

--- classnotes/week40/sketch.js
+++ classnotes/week40/sketch.js
@@ -27,7 +27,7 @@ topImage = loadImage("assets/mnttop.png");
 
 function setup() {
   // put setup code here
-  createCanvas(1440,754);
+  createCanvas(900,700);
   noStroke(0);
   seasons = createSelect();
   seasons.position(17, 17,50);
@@ -37,7 +37,7 @@ function setup() {
   seasons.option("Leaves");
   seasons.option("Let it Snow!");
   seasons.option("Day to Night");
-  canvas = createCanvas(1440,754);
+  canvas = createCanvas(900,700);
   seasons.changed(function() {
   seasonType = seasons.value();
   });
