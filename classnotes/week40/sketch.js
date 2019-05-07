30b86932ef1ac3053e092ede02f5e6be4560ab8a
lauren <lns44@miami.edu>
2019-05-07 02:21:44 -0400

    a

--- classnotes/week40/sketch.js
+++ classnotes/week40/sketch.js
@@ -11,12 +11,18 @@ var bubbleLocationY = [];
 var bubbleImage;
 var bubbleAmount = 100;
 
+var snowLocationX = [];
+var snowLocationY = [];
+var snowAmount = 150;
+
 var backImage;
 
 function preload() {
 bubbleImage = loadImage("assets/bubble.png");
 leafImage = loadImage("assets/leaf.png");
 backImage = loadImage("assets/mnt.jpg");
+moonImage = loadImage("assets/moon.png");
+topImage = loadImage("assets/mnttop.png");
 }
 
 function setup() {
@@ -28,6 +34,8 @@ function setup() {
   seasons.option("Click here to change up the scene!");
   seasons.option("Leaves");
   seasons.option("Bubbles");
+  seasons.option("Let it Snow!");
+  seasons.option("Day to Night");
   canvas = createCanvas(1440,754);
   seasons.changed(function() {
   seasonType = seasons.value();
@@ -44,13 +52,20 @@ function setup() {
     bubbleLocationY[i] = random(0, -400);
   }
 
+  for (var i = 0; i < snowAmount; i++) {
+    snowLocationX[i] = random(0, width);
+    snowLocationY[i] = random(0, -500);}}
+
 function draw() {
 background(255);
 
 image(backImage,0,0, backImage.width/4.5,backImage.height/4.5);
 
 
+
+
 if (seasonType == "Click here to change up the scene!") {
+
 
 }if (seasonType == "Leaves") {
     textSize(12);
@@ -73,7 +88,28 @@ if (seasonType == "Click here to change up the scene!") {
 
       if(bubbleLocationY[i] < height - 20){
         bubbleLocationY[i]++;
-        bubbleLocationX[i] = bubbleLocationX[i] + sin(radians(frameCount));
-}}}}
+        bubbleLocationX[i] = bubbleLocationX[i] + sin(radians(frameCount));}}
+}else if (seasonType == "Day to Night") {
+
+  if(mouseX> height/2){
+    console.log("Below Line");
+    image(moonImage, mouseX, 20, 150, 150);
+  }
+  if(mouseX< height/2){
+    console.log("Above Line");
+
+    image(sunImage, mouseX, 20, 150, 150)};
+    image(topImage,276,94,260,100);
+
+ }else if (seasonType == "Let it Snow!") {
+  for (var i = 0; i < snowAmount; i++) {
+    snowLocationY[i]++;
+    ellipse(snowLocationX[i], snowLocationY[i], 4,4);
+
+    if(snowLocationY[i] > height){
+      snowLocationY[i] = random(-50,0);
+      snowLocationX[i] = random(0,width);}
+
+    }
   }
 }
