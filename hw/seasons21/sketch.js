1017587e59860cf3662b740032b071057dd7284c
lauren <lns44@miami.edu>
2019-05-06 20:41:23 -0400

    a

--- hw/seasons21/sketch.js
+++ hw/seasons21/sketch.js
@@ -41,6 +41,8 @@ function draw() {
 background(bg);
 
 
-  if(mouseX > 18 && mouseX < 71 && mouseY > 59 && mouseY < 101){
+
+if(mouseX > 18 && mouseX < (71) && mouseY > 59 && mouseY < 101){
   if(mouseIsPressed == true){
-  fruit = snakeArray [0];}
+    fruit = snakeArray[0];
+  }
