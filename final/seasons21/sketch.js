f4c9492a118ca8940552b3294b1264c02578ebc2
lauren <lns44@miami.edu>
2019-05-06 23:57:15 -0400

    ,

--- final/seasons21/sketch.js
+++ final/seasons21/sketch.js
@@ -6,20 +6,10 @@ var snakebutton;
 var currentImage = 0;
 
 function preload(){
-snake = loadImage("assets/snake.png");
 background = loadImage("assets/background.jpg");
 }
 function setup() {
 createCanvas(800,600);
 background = loadImage("assets/background.jpg");
-snakebutton = createButton("snake");
-snakebutton.position(10,40);
-snakebutton.mousePressed(function(){
 currentImage = 0;
 });
-
-function draw() {
-
-if(currentImage == 0){
-  image(snake, 0,20, snake.width/2, snake.height/2);
-}}
